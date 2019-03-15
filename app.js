e146ac0135bb63e83083b872c0d5fd06fb2137e4
Katelyn Williamson <katelynrwilliamson@gmail.com>
2019-03-14 20:32:49 -0600

    finished functinoality

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ var rock = 0
 var paper = 1
 var scissor = 2
 var user_input
-
 var computer_choice
 
 function generate_computer_choice(){
@@ -37,23 +36,60 @@ function user_choice() {
     
 //conditional to determine winner if/else
 function determine_winner() {
-    console.log("User Input:" + user_input)
-    console.log("Computer Input:" + computer_choice)
+    // console.log("User Input:" + user_input)
+    // console.log("Computer Input:" + computer_choice)
     user_choice()
     generate_computer_choice()
     console.log("User Input:" + user_input)
     console.log("Computer Input:" + computer_choice)
-    if (computer_choice > user_input) {
-        // document.getElementsByClassName("result").innerHTML = "Computer Wins!"
-        alert("Computer Wins!")
+
+    if (computer_choice == 0 &&  user_input == 0 ) {
+        document.getElementById("result").innerHTML = "You Tie!"
+        // alert("You Tie!")
 
     }
-    else if (computer_choice === user_input){
-        document.getElementsByClassName("result").innerHTML = "You Tie!"
-        
+    else if (computer_choice == 0 && user_input == 1 ){
+        document.getElementById("result").innerHTML = "You Lose!"
+        // alert("You Lose!")
     }
+    else if(computer_choice == 0 && user_input == 2){
+      document.getElementById("result").innerHTML = "Computer Wins!"
+      // alert("Computer Wins!")
+    }
+
+
+    else if (computer_choice == 1 &&  user_input == 0 ) {
+      document.getElementById("result").innerHTML = "Computer Wins!"
+      // alert("Computer Wins!")
+
+    }
+    else if (computer_choice == 1 && user_input == 1 ){
+      document.getElementById("result").innerHTML = "You Tie!"
+      // alert("You Tie!")
+    }
+    else if(computer_choice == 1 && user_input == 2){
+      document.getElementById("result").innerHTML = "You Win!"
+      // alert("You Win!")
+    }
+
+
+    else if (computer_choice == 2 &&  user_input == 0 ) {
+      document.getElementById("result").innerHTML = "You Win!"
+      // alert("You Win!")
+
+    }
+    else if (computer_choice == 2 && user_input == 1 ){
+      document.getElementById("result").innerHTML = "Computer Wins!"
+      // alert("Computer Wins!")
+    }
+    else if(computer_choice == 2 && user_input == 2){
+      document.getElementById("result").innerHTML = "You Tie!"
+      // alert("You Tie!")
+    }
+    
     else {
-    document.getElementsByClassName("result").innerHTML = "You Lose!"
+    // document.getElementsByClassName("result").innerHTML = "You Lose!"
+    alert("Error")
     }
 }
 
